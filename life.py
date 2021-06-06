bdef96631b6d3039fc01cae7c570f1b8852c90a4
Gabe Braden <gabebraden2003@gmail.com>
2021-06-06 00:46:12 -0400

    Added function to spawn next generation

--- life.py
+++ life.py
@@ -1,20 +1,19 @@
-def set_matrix():
+def set_matrix(matrix=[[0, 1, 0], [0, 0, 1], [1, 1, 1]]):
     """Set a 8x8 matrix for testing purposes
+        Parameters
+        ----------
+        matrix: list
+            The matrix to set, the default is the seed for the glider shape
 
         Returns
         ---------
         list
-                8x8 2D matrix
+            8x8 2D matrix
         """
-    return ([
-        [1, 0, 0, 1],
-        [0, 1, 0, 1],
-        [1, 0, 0, 0],
-        [0, 1, 0, 0],
-    ])
+    return matrix
 
 
-def get_cell(matrix, y, x):
+def get_cell(matrix, x, y):
     """Validates cell coordinates
 
     Parameters
@@ -35,14 +34,14 @@ def get_cell(matrix, y, x):
     """
     if (x >= 0) and (y >= 0):
         try:
-            value = matrix[y][x]
+            value = matrix[x][y]
         except:
             value = None
 
         return value
 
 
-def get_neighbours(matrix, y, x):
+def get_neighbours(matrix, x, y):
     """Gets all the inputted cell's neighbours
 
     Parameters
@@ -60,18 +59,18 @@ def get_neighbours(matrix, y, x):
             returns a list with all the values of the neighbouring cells
     """
     return([
-        get_cell(matrix, y-1, x),  # up
-        get_cell(matrix, y-1, x+1),  # up, right
-        get_cell(matrix, y, x+1),  # right
-        get_cell(matrix, y+1, x+1),  # down, right
-        get_cell(matrix, y+1, x),  # down
-        get_cell(matrix, y+1, x-1),  # down, left
-        get_cell(matrix, y, x-1),  # left
-        get_cell(matrix, y-1, x-1),  # up, left
+        get_cell(matrix, x, y-1),  # up
+        get_cell(matrix, x+1, y-1),  # up, right
+        get_cell(matrix, x+1, y),  # right
+        get_cell(matrix, x+1, y+1),  # down, right
+        get_cell(matrix, x, y+1),  # down
+        get_cell(matrix, x-1, y+1),  # down, left
+        get_cell(matrix, x-1, y),  # left
+        get_cell(matrix, x-1, y-1),  # up, left
     ])
 
 
-def determine_fate(matrix, y, x):
+def determine_fate(matrix, x, y):
     """Determines wheter the cell inputted cell will live to the next generation
     Rules:
     Any live cell with two or three live neighbours survives.
@@ -90,21 +89,37 @@ def determine_fate(matrix, y, x):
         Returns
         -------
         fate: str
-            The function returns 'Live' if the cell will live onto the next generation
-            'Dead' is returned if te cell will die
+            The function returns 1 if the cell will live onto the next generation
+            0 is returned if te cell will die
     """
-    neighbours = get_neighbours(matrix, y, x)
+    neighbours = get_neighbours(matrix, x, y)
     num_of_live_neighbours = 0
     for num in neighbours:
         if num != None:
             num_of_live_neighbours = num_of_live_neighbours + num
-    if get_cell(matrix, y, x) == 1:
+    if get_cell(matrix, x, y) == 1:
         if (num_of_live_neighbours == 2) or (num_of_live_neighbours == 3):
-            return 'Live'
+            return 1
         else:
-            return 'Dead'
+            return 0
     else:
         if num_of_live_neighbours == 3:
-            return 'Live'
+            return 1
         else:
-            return 'Dead'
+            return 0
+
+
+def next_gen(current_gen):
+    gen = []
+    for row in current_gen:
+        row.insert(0, 0)
+        row.append(0)
+    dead_row = [0 for cell in current_gen[0]]
+    current_gen.insert(0, dead_row)
+    current_gen.append(dead_row)
+    for x in range(len(current_gen)):
+        row = []
+        for y in range(len(current_gen[x])):
+            row.append(determine_fate(current_gen, x, y))
+        gen.append(row)
+    return gen
